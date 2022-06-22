cf815d7e178fdc28a74ff602fa33c3a7951cce34
Sean Murray <disbesean@gmail.com>
2022-06-22 14:46:39 -0700

    fixing bug where chart filter does not include year

--- client/src/components/Analytics/ExpenseLineChart.js
+++ client/src/components/Analytics/ExpenseLineChart.js
@@ -1,7 +1,16 @@
 import React, {useState, useEffect} from 'react';
+import moment from 'moment';
 import { Line } from 'react-chartjs-2';
 import {getMonthsArray, monthNames} from '../../utils/common';
 import { ExpenseCategories } from '../../utils/BudgetCategories';
+
+function filterByMonth(budgetData, month) {
+    budgetData.filter((ele) => moment(ele.budgetDate).month() === month);
+}
+
+function filterAndSumByCategory(budgetData, category) {
+    budgetData.filter((ele) => ele.budgetCategory === category.value).reduce((sum, curr) => sum + curr.budgetAmount, 0);
+}
 
 export default function ExpenseLineChart({budget, type}) {
     const [data, setData] = useState([]);
@@ -25,38 +34,40 @@ export default function ExpenseLineChart({budget, type}) {
             },
         }
     };
-  useEffect(async () => {
-    const filteredBudget = typeof(budget.data)==='undefined'?[]:budget.data.filter(ele => ele.budgetType===type);
-    const intervalList = typeof(budget.data)==='undefined'?[]:getMonthsArray();
-    console.log(intervalList);
-    filteredBudget.map(ele=>{ele.filteredDate = monthNames[new Date(ele.budgetDate).getMonth()]});
-    const totals = intervalList.map(monthEle => {
-        const sum = filteredBudget.filter(e => e.filteredDate===monthEle).reduce((sum, curr) => sum + curr.budgetAmount, 0);
-        const categoryTotals = ExpenseCategories.map(categoryEle => {
-            const categoryTotal = filteredBudget.filter(e => e.filteredDate===monthEle && e.budgetCategory===categoryEle.value).reduce((sum, curr) => sum + curr.budgetAmount, 0);
-            return {expenseCategory: categoryEle.value, categoryTotal:categoryTotal.toFixed(2)};
+  useEffect(() => {
+    if (typeof(budget.data) !=='undefined') {
+        const filteredBudget = budget.data.filter(ele => ele.budgetType===type);
+        const intervalList = getMonthsArray();
+        const totals = intervalList.map(interval => {
+            const sum = filteredBudget.filter(e => moment(e.budgetDate).month()===interval.month && moment(e.budgetDate).year()===interval.year).reduce((sum, curr) => sum + curr.budgetAmount, 0);
+            const categoryTotals = ExpenseCategories.map(categoryEle => {
+                const categoryTotal = filteredBudget.filter(e => moment(e.budgetDate).month()===interval.month && moment(e.budgetDate).year()===interval.year && e.budgetCategory===categoryEle.value).reduce((sum, curr) => sum + curr.budgetAmount , 0);
+                return {expenseCategory: categoryEle.value, categoryTotal:categoryTotal.toFixed(2)};
+            });
+            return {month: monthNames[interval.month], total: sum, categoryTotals: categoryTotals};
         });
-        return {month: monthEle, total: sum, categoryTotals: categoryTotals};
-    });
-    const datasets = ExpenseCategories.map(categoryEle=> {
-        return {
-            label: categoryEle.value, 
-            data: totals.map(ele=>ele.categoryTotals.find(ele => ele.expenseCategory === categoryEle.value).categoryTotal),
-            backgroundColor: categoryEle.color,
-            borderColor: categoryEle.color,
-        }
-    });
-    datasets.unshift({
-        label: 'Total',
-        data: totals.map(ele=>ele.total),
-        backgroundColor: 'purple',
-        borderColor: 'purple',
-    });
-    // Set chart data and colors
-    setData({
-        labels: intervalList,
-        datasets: datasets,
-    });
+        console.log(intervalList);
+        console.log(totals)
+        const datasets = ExpenseCategories.map(categoryEle=> {
+            return {
+                label: categoryEle.value, 
+                data: totals.map(ele=>ele.categoryTotals.find(ele => ele.expenseCategory === categoryEle.value).categoryTotal),
+                backgroundColor: categoryEle.color,
+                borderColor: categoryEle.color,
+            }
+        });
+        datasets.unshift({
+            label: 'Total',
+            data: totals.map(ele => ele.total),
+            backgroundColor: 'purple',
+            borderColor: 'purple',
+        });
+        // Set chart data and colors
+        setData({
+            labels: intervalList.map(ele => monthNames[ele.month]),
+            datasets: datasets,
+        });
+    }
 }, [budget]);
 
   return (
